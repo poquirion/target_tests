a176ba4dde187bc61eff886f66166ff675e688dd
Pierre-Olivier Quirion <pioliqui@gmail.com>
2015-08-01 00:03:54 -0400

    Automatically built target

---
+++ logs/tmp/pipeline_manager.sh
@@ -0,0 +1,2 @@
+"/usr/bin/octave" --silent --eval "load('/home/poquirion/simexp/work/result/logs//PIPE.mat','path_work'), if ~ismember(path_work,{'gb_niak_omitted','gb_psom_omitted'}), path(path_work), end, load('/home/poquirion/simexp/work/result/logs//PIPE_manager_opt.mat'), psom_pipeline_process('/home/poquirion/simexp/work/result/logs//PIPE.mat',opt),exit" >"/home/poquirion/simexp/work/result/logs//PIPE.log" 2>&1
+touch "/home/poquirion/simexp/work/result/logs//PIPE.exit"
